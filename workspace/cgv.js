db53d2916161a2bb318d9e5d366bed9d2558daff
ahseonga <ahseonga@naver.com>
2024-07-28 15:18:26 +0900

    240728 마지막 섹션 추가

--- workspace/cgv.js
+++ workspace/cgv.js
@@ -1,7 +1,18 @@
-// --- 메인 섹션 4 --- //
+// --- 메인 섹션 4 특별관 사진 --- //
+document.addEventListener('DOMContentLoaded', function() {
+    const TextElements = document.querySelectorAll('.picture-strongtext');
 
+    TextElements.forEach(function(element) {
 
+        element.addEventListener('mouseover', function(event) {
 
+            const newImgSrc = element.getAttribute('data-img');
+            const mainImage = document.getElementById('sectionImge-1');
+
+            mainImage.setAttribute('src', newImgSrc);
+        });
+    });
+});
 
 // --- footer 부분 계열사 Go --- //
 
